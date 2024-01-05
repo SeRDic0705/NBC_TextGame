989420acd84cfd9a68aedc98a7eca323e58d5234
SeRDic0705 <serdic0705@gmail.com>
2024-01-05 17:19:53 +0900

    feat: 메인 화면 기능

---
+++ NBC_TextGame/Shop.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace NBC_TextGame
+{
+    internal class Shop
+    {
+        public List<Item> items = new List<Item>();
+
+        public void AddItem(Item item)
+        {
+            this.items.Add(item);
+        }
+
+        public void BuyItem(int index, Character player)
+        {
+            if (items[index].price >  player.gold)
+            {
+                Console.WriteLine("Gold가 부족합니다.");
+            }
+            else
+            {
+                player.gold -= items[index].price;
+                player.AddInventory(items[index]);
+                items.RemoveAt(index);
+                SaveOptions();
+            }
+        }
+
+
+        public void SaveOptions()
+        {
+            string path = System.IO.Directory.GetCurrentDirectory() + "/shop.json";
+            string json = JsonConvert.SerializeObject(items, Formatting.Indented);
+            File.WriteAllText(path, json);
+        }
+
+        public void LoadOptions()
+        {
+            string path = System.IO.Directory.GetCurrentDirectory() + "/shop.json";
+            string json = File.ReadAllText(path);
+
+            List<Item> items = JsonConvert.DeserializeObject<List<Item>>(json);
+
+            this.items = items;
+
+        }
+    }
+}
